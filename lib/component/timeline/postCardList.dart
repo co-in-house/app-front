edf5d921265a81b9b92d8f0d9a35e8d2ffe6be1b
yota konno <yota.rock.guitar@gmail.com>
2021-06-15 23:51:48 +0900

    add launch page

--- lib/component/timeline/postCardList.dart
+++ lib/component/timeline/postCardList.dart
@@ -1,15 +1,34 @@
 import 'package:Inhouse/component/timeline/postCard.dart';
 import 'package:Inhouse/model/postList.dart';
+import 'package:Inhouse/util/util.dart';
 import 'package:flutter/material.dart';
 
 class PostCardList extends SliverChildListDelegate {
-  PostCardList(PostList postList) : super(_PostList.buildPostList(postList));
+  PostCardList(BuildContext context, PostList postList)
+      : super(_PostList.buildPostList(context, postList));
+  BuildContext context;
 }
 
 class _PostList {
-  static List<Widget> buildPostList(PostList postList) {
+  static List<Widget> buildPostList(BuildContext context, PostList postList) {
     print("buildPostList");
-    if (postList.contentsList == null || postList.contentsList.length == 0) {
+    if (postList.contentsList == null) {
+      return [
+        Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width *
+                Const.containerWidthPercentage,
+            height: MediaQuery.of(context).size.height * 0.7,
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  LinearProgressIndicator(),
+                ]),
+          ),
+        ),
+      ];
+    } else if (postList.contentsList.length == 0) {
       return [Text("No Content")];
     } else {
       List<Widget> list = [];
--- lib/view/rootFlame.dart
+++ lib/view/rootFlame.dart
@@ -3,6 +3,7 @@ import 'package:Inhouse/component/navBar.dart';
 import 'package:Inhouse/component/drawer.dart';
 import 'package:Inhouse/model/routingState.dart';
 import 'package:Inhouse/service/api/getLocationListService.dart';
+import 'package:Inhouse/service/api/getPostListService.dart';
 import 'package:Inhouse/service/api/getTagListService.dart';
 import 'package:Inhouse/util/wrapper.dart';
 import 'package:Inhouse/view/eventPage.dart';
@@ -27,7 +28,10 @@ class RootFlame extends StatelessWidget {
   Widget build(BuildContext context) {
     return StatefulWrapper(
       onInit: () async {
+        // 初期API call
         // master list 取得
+        print('Async Master List Load ');
+        await context.read<GetPostListService>().call();
         await context.read<GetTagListService>().call();
         await context.read<GetLocationListService>().call();
         print('Async Master List Load Done');
