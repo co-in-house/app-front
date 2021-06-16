661b683c8dffd21baa2b97ea0c8651f73fe06872
yota konno <yota.rock.guitar@gmail.com>
2021-06-17 01:23:21 +0900

    画像選択機能 追加

--- lib/view/newCommunity/newCommunityIconPage.dart
+++ lib/view/newCommunity/newCommunityIconPage.dart
@@ -1,32 +1,54 @@
-// import 'dart:io';
+import 'dart:io';
+import 'package:Inhouse/model/newCommunityInfo.dart';
 import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 class NewCommunityIconPage extends StatefulWidget {
+  NewCommunityIconPage({Key key, this.newCommunityInfo}) : super(key: key);
+  final NewCommunityInfo newCommunityInfo;
   @override
   _NewCommunityIconState createState() => _NewCommunityIconState();
 }
 
 class _NewCommunityIconState extends State<NewCommunityIconPage> {
-  // File _image;
-  // final picker = ImagePicker();
+  File _image;
+  final picker = ImagePicker();
 
   Future getImageFromCamera() async {
-    // final pickedFile = await picker.getImage(source: ImageSource.camera);
-
-    // setState(() {
-    //   _image = File(pickedFile.path);
-    // });
-    // print("path : " + pickedFile.path);
+    try {
+      final pickedFile = await picker.getImage(source: ImageSource.camera);
+      setState(() {
+        if (pickedFile != null) {
+          _image = File(pickedFile.path);
+          print("path : " + pickedFile.path);
+        } else {
+          print('No image selected.');
+        }
+      });
+    } on PlatformException catch (err) {
+      print(err);
+    } on Exception catch (err) {
+      print(err);
+    }
   }
 
   Future getImageFromGallery() async {
-    // final pickedFile = await picker.getImage(source: ImageSource.gallery);
-
-    // setState(() {
-    //   _image = File(pickedFile.path);
-    // });
-    // print("path : " + pickedFile.path);
+    try {
+      final pickedFile = await picker.getImage(source: ImageSource.gallery);
+      setState(() {
+        if (pickedFile != null) {
+          _image = File(pickedFile.path);
+          print("path : " + pickedFile.path);
+        } else {
+          print('No image selected.');
+        }
+      });
+    } on PlatformException catch (err) {
+      print(err);
+    } on Exception catch (err) {
+      print(err);
+    }
   }
 
   @override
@@ -39,28 +61,28 @@ class _NewCommunityIconState extends State<NewCommunityIconPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // Padding(
-            //   padding: const EdgeInsets.all(8.0),
-            //   child: Container(
-            //       width: 300,
-            //       child: _image == null
-            //           ? Text('No image selected.')
-            //           : Image.file(_image)),
-            // ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                FloatingActionButton(
+                ElevatedButton(
                   onPressed: getImageFromCamera,
-                  tooltip: 'Pick Image From Camera',
+                  // tooltip: 'Pick Image From Camera',
                   child: Icon(Icons.add_a_photo),
                 ),
-                FloatingActionButton(
+                ElevatedButton(
                   onPressed: getImageFromGallery,
-                  tooltip: 'Pick Image From Gallery',
+                  // tooltip: 'Pick Image From Gallery',
                   child: Icon(Icons.photo_library),
                 ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  width: 300,
+                  child: _image == null
+                      ? Text('No image selected.')
+                      : Image.file(_image)),
             )
           ],
         ),
