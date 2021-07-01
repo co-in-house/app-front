cfbee51bd23472605330d296e3fbbec1b715881d
yota konno <yota.rock.guitar@gmail.com>
2021-07-02 02:30:38 +0900

    コミュニティ作成後、再描画させないように修正

--- lib/component/community/communityInfoCardList.dart
+++ lib/component/community/communityInfoCardList.dart
@@ -2,8 +2,11 @@ import 'package:Inhouse/component/community/communityContentCard.dart';
 import 'package:Inhouse/component/community/communityNoteCard.dart';
 import 'package:Inhouse/component/community/communityRequirementCard.dart';
 import 'package:Inhouse/component/community/communityTagCard.dart';
+import 'package:Inhouse/main.dart';
 import 'package:Inhouse/model/newCommunityInfo.dart';
+import 'package:Inhouse/service/api/postNewCommunityService.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CommunityInfoCardList extends SliverChildListDelegate {
   CommunityInfoCardList(BuildContext context, NewCommunityInfo newCommunityInfo)
@@ -24,10 +27,17 @@ class _CommunityInfoCardList {
         CommunityRequirementCard(requirement: newCommunityInfo.requirement));
     list.add(CommunityNoteCard(note: newCommunityInfo.note));
     list.add(
-      SizedBox(
-        height: MediaQuery.of(context).size.height * 0.05,
+      ElevatedButton(
+        child: Text("submit"),
+        style: ElevatedButton.styleFrom(elevation: 16),
+        onPressed: () async {
+          print("submit");
+          await context.read<PostNewCommunityService>().call(newCommunityInfo);
+          Navigator.popUntil(context, (route) => route.isFirst);
+        },
       ),
     );
+    list.add(SizedBox(height: MediaQuery.of(context).size.height * 0.05));
     return list;
   }
 }
