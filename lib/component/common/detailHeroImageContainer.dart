1bcc5e1cbb9adb3f7a59c9af273dbdc26e5c2ea5
yota konno <yota.rock.guitar@gmail.com>
2021-10-13 01:23:06 +0900

    fix event detail hero image elevation

--- lib/component/common/detailHeroImageContainer.dart
+++ lib/component/common/detailHeroImageContainer.dart
@@ -1,3 +1,4 @@
+import 'package:Inhouse/util/util.dart';
 import 'package:flutter/material.dart';
 
 class DetailHeroImageContainer extends StatelessWidget {
@@ -6,17 +7,36 @@ class DetailHeroImageContainer extends StatelessWidget {
       : super(key: key);
   final String imgUrl;
   final String heroTag;
-
+  final double _radius = 25;
   @override
   Widget build(BuildContext context) {
     return Hero(
       tag: this.heroTag,
-      child: Container(
-        height: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(this.imgUrl),
-            fit: BoxFit.fitWidth,
+      child: Card(
+        elevation: 30.0,
+        margin: EdgeInsets.only(
+          top: 0,
+          bottom: 30,
+          left: 0,
+          right: 0,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(_radius),
+            bottomRight: Radius.circular(_radius),
+          ),
+        ),
+        child: Container(
+          height: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(_radius),
+              bottomRight: Radius.circular(_radius),
+            ),
+            image: DecorationImage(
+              image: NetworkImage(this.imgUrl),
+              fit: BoxFit.fitWidth,
+            ),
           ),
         ),
       ),
